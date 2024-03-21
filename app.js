d2ebdf99a6139a22025f65243b81e36d36969807
Mukhiddin06 <muhiddinbegiboyev@gmail.com>
2024-03-21 06:52:43 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,114 @@
+// misol - 1
+// let num1 = prompt("son kiriting")
+// const primeNumber = (number)=>{
+//     let count = 0
+//     for(let i = 0; i <= number; i++){
+//         if(number % i == 0){
+//             count++
+//         }
+//     }
+//     if(count > 2){
+//         console.log("tub emas")
+//     }
+//     else if(count == 2){
+//         console.log("tub son")
+//     }
+// }
+// primeNumber(num1)
+
+
+
+// misol - 2
+// let num = 6
+
+// const findPerfectNum = function(number){
+//     let total = 0
+//     for (let i = 0; i < number; i++) {
+//         if(number % i == 0){
+//             total += i
+//         }
+//     }
+//     if(number == total){
+//         console.log("Perfect Number")
+//     }
+//     else{
+//         console.log("not a perfect number")
+//    }
+// }
+// findPerfectNum(num)
+
+
+
+// misol - 3
+// let arr = "lesson"
+
+// function findLetter(array){
+//     let count = 0
+//     let i = 0
+//     while (i <= array.length) {
+//         let a = array[i]
+//     if(a === "a"|| a === "i"|| a === "e"||a ==="o" || a === "o'" || a === "u"){
+//         count++
+//     }
+//     i++
+//     }
+//     console.log(count + " ta unli")
+//     console.log(array.length - count + " ta undosh")
+// }
+// findLetter(arr)
+
+
+// misol - 4
+// let number1 = 4
+// let number2 = 7
+
+// function aboutFive(num1, num2){
+//     let a = Math.abs(num1 - 5)
+//     let b = Math.abs(num2 - 5)
+//     if(a > b){
+//         console.log(num2)
+//     }
+//     else if(b > a){
+//         console.log(num1)
+//     }
+// }
+// aboutFive(number1, number2)
+
+
+// misol - 5
+// let num1 = 5
+// let num2 = 4
+
+// function rectangle(a, b){
+//     console.log(a*b + " = yuzasi");
+//     console.log((a + b) * 2 + " = perimetri")
+// }
+// rectangle(num1, num2)
+
+
+// misol - 6
+// let num1 = 5
+// let num2 = 3
+
+// function power(a, n){
+//     return console.log(a ** n)
+// }
+// power(num1, num2)
+
+
+
+// misol - 7
+// let number = -6
+
+// function sign(n){
+//     if (n == 0){
+//         console.log(0)
+//     }
+//     else if (n > 0){
+//         console.log(1)
+//     }
+//     else{
+//         console.log(-1)
+//     }
+// }
+// sign(number)
